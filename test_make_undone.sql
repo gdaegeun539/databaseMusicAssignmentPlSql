48ed1df827a748d1fea8198c3cda0968098322a5
justin6043 <79126108+justin6043@users.noreply.github.com>
2021-11-25 10:47:48 +0900

    랜덤함수 추가

--- test_make_undone.sql
+++ test_make_undone.sql
@@ -1,8 +1,8 @@
 -- 음악 정보 뷰
-create view music_view (musicname, playtime, singer, writer, composer, albumname, publishdate, genrename)
-as select mu.musicid mu.musicname, mu.playtime, mk.singer, mk.writer, mk.composer, a.albumname, a.publishdate, g.genrename
+create view music_view (musicid, musicname, playtime, singer, writer, composer, albumname, publishdate, genrename)
+as select mu.musicid, mu.musicname, mu.playtime, mk.singer, mk.writer, mk.composer, a.albumname, a.publishdate, g.genrename
 from music mu, genre g, maker mk, album a
-where mu.musicid = mk.musicid and mu.albumid = album.albumid and m.genreid = g.genreid
+where mu.musicid = mk.musicid and mu.albumid = a.albumid and mu.genreid = g.genreid
 with read only;
 
 -- 유저 정보 뷰
@@ -12,7 +12,7 @@ from userinfo u, membership m, usermembership um
 where u.usernum = um.userid and um.membershipid = m.membershipid and u.countryid = m.countryid;
 
 -- 이용자 회원가입
-insert into userinfo values (랜덤함수, '입력값', '입력값','입력값','입력값', '0000-00-00', '입력값','KOR');
+insert into userinfo values (trunc(dbms_random.value(1,1000000)), '입력값', '입력값','입력값','입력값', '0000-00-00', '입력값','KOR');
 
 -- 이용자 로그인
 select *
@@ -34,13 +34,15 @@ where userid = 확인된유저변수값;
 
 -- 재생목록 만들기
 insert into playlist(userid, plid, plname, is_public, createdate)
-select u.usernum, 랜덤값함수, 클라이언트입력값, 0, 현재날짜값
+select u.usernum, trunc(dbms_random.value(1, 10000000)), 클라이언트입력값, 0, 현재날짜값
 from userinfo u
 where u.usernum = 현재유저변수값;
 -- (공개여부 변경시)
 update playlist
 set is_public = 1
-where u.usernum = 현재유저변수값 and p.plid = 현재재생목록값(클라이언트);
+where p.plid = 현재재생목록값(클라이언트) and exists(select usernum
+                                                    from userinfo
+                                                    where usernum = 현재유저변수값);
 -- 곡 추가[미완/정상작동보장x]
 insert into music_playlist(plid, musicid, priority)
 select p.plid, m.musicid, max(p.priority)+1
@@ -80,7 +82,7 @@ where userfname = 입력값 and userlname = 입력값 and bday = 입력값
 
 -- 고객지원
 insert into support
-select 랜덤값, u.usernum, st.inquiry_code, 현재시간, 0
+select trunc(dbms_random.value(1,1000000)), u.usernum, st.inquiry_code, 현재시간, 0
 from userinfo u, support_type st
 where u.usernum = 현재유저변수값 or inquiry_reason = 클라이언트선택값;
 -- 문의처리여부 변경
@@ -90,7 +92,7 @@ update support
 
 -- 계정탈퇴요청
 insert into support
-select 랜덤값, u.usernum, 2, 현재시간, 0 -- 코드2번 계정삭제
+select trunc(dbms_random.value(1,1000000)), u.usernum, 2, 현재시간, 0 -- 코드2번 계정삭제
 from userinfo u
 where u.usernum = 현재유저변수값;
 -- 계정탈퇴처리
@@ -105,14 +107,14 @@ insert into genre
 values(1, 'Rock') -- 장르번호는 임의지정
 -- 앨범추가
 declare
-rand_album_id int := 랜덤함수;
+rand_album_id int := trunc(dbms_random.value(1,1000));
 begin
 insert into album
 values (rand_album_id, '앨범명', 2000/01/01);
 end;
 -- 음원추가
 declare
-rand_music_id int := 랜덤함수;
+rand_music_id int := trunc(dbms_random.value(1,10000));
 begin
 insert into music
 select rand_music_id, '노래이름', 180, a.albumid, g.genreid
